b1c23a8265352876e2cfe994b90e71dc48b3fcc8
Rodrigo Pedro <rodrigopedro@gmail.com>
2023-01-29 17:25:41 -0300

    Changes

--- app/src/main/java/com/qualapecompose/MainActivity.kt
+++ app/src/main/java/com/qualapecompose/MainActivity.kt
@@ -3,7 +3,6 @@ package com.qualapecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
